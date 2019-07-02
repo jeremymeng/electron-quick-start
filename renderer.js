f16bf1595f293ddc983202ffd311a6e4cb091200
Matthew Rayermann <marayerm@microsoft.com>
2019-07-02 14:56:28 -0700

    Proof of concept for child system dialog escape bubbling up

--- renderer.js
+++ renderer.js
@@ -1,3 +1,19 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const { remote } = require('electron');
+var count = 0;
+
+window.addEventListener("keyup", (e) => {
+    if (e.keyCode === 27) {
+        document.getElementById(`test-span`).innerText = `Count: ${++count}`;
+    }
+});
+
+document.getElementById(`test-a`).onclick = function () {
+    var callback = function() {
+        console.log("Dialog closed!");
+    };
+    remote.dialog.showOpenDialog(remote.BrowserWindow.getFocusedWindow(), {}, callback);
+}
